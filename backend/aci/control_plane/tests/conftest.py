43c3487d2442cdec8385b2109053e7667b88fc24
Kelvin Wong <kelvin@aipolabs.xyz>
2025-08-27 10:49:50 +0100

    feat: adding basic dummies

--- backend/aci/control_plane/tests/conftest.py
+++ backend/aci/control_plane/tests/conftest.py
@@ -13,82 +13,75 @@ from aci.common.schemas.auth import ActAsInfo
 from aci.common.test_utils import clear_database, create_test_db_session
 from aci.control_plane import dependencies as deps
 from aci.control_plane.main import app as fastapi_app
-
-
-# ------------------------------------------------------------
-#
-# Test Clients
-#
-# ------------------------------------------------------------
-@pytest.fixture(scope="function")
-def test_client(
-    test_client_admin: TestClient, test_client_member: TestClient
-) -> Generator[TestClient, None, None]:
-    """
-    Alias for test_client_admin
-    """
-    yield test_client_admin
+from aci.control_plane.routes.auth import _sign_token
 
 
 @pytest.fixture(scope="function")
-def test_client_admin(
-    db_session: Session, dummy_admin: User, dummy_organization: Organization
-) -> Generator[TestClient, None, None]:
-    """
-    Test Client with a admin request context.
-    """
-    fastapi_app.dependency_overrides[deps.get_request_context] = lambda: deps.RequestContext(
-        db_session=db_session,
-        user_id=dummy_admin.id,
-        act_as=ActAsInfo(
-            organization_id=dummy_organization.id,
-            role=OrganizationRole.ADMIN,
-        ),
-    )
+def test_client(db_session: Session) -> Generator[TestClient, None, None]:
+    fastapi_app.dependency_overrides[deps.get_request_context] = lambda: db_session
     # disable following redirects for testing login
     # NOTE: need to set base_url to http://localhost because we set TrustedHostMiddleware in main.py
     with TestClient(fastapi_app, base_url="http://localhost", follow_redirects=False) as c:
         yield c
 
 
+# ------------------------------------------------------------
+# Dummy Access Tokens for testing
+# - dummy_access_token_admin
+# - dummy_access_token_admin_act_as_member
+# - dummy_access_token_member
+# - dummy_access_token_no_orgs (without act as)
+# ------------------------------------------------------------
 @pytest.fixture(scope="function")
-def test_client_admin_act_as_member(
-    db_session: Session, dummy_admin: User, dummy_organization: Organization
-) -> Generator[TestClient, None, None]:
-    """
-    Test Client with a admin request context but act as a member.
-    """
-    fastapi_app.dependency_overrides[deps.get_request_context] = lambda: deps.RequestContext(
-        db_session=db_session,
-        user_id=dummy_admin.id,
-        act_as=ActAsInfo(
-            organization_id=dummy_organization.id,
-            role=OrganizationRole.MEMBER,
-        ),
+def dummy_access_token_admin(dummy_admin: User) -> str:
+    org_membership = dummy_admin.organization_memberships[0]
+    return _sign_token(
+        dummy_admin,
+        ActAsInfo(organization_id=org_membership.organization_id, role=OrganizationRole.ADMIN),
     )
-    # disable following redirects for testing login
-    # NOTE: need to set base_url to http://localhost because we set TrustedHostMiddleware in main.py
-    with TestClient(fastapi_app, base_url="http://localhost", follow_redirects=False) as c:
-        yield c
 
 
 @pytest.fixture(scope="function")
-def test_client_member(
-    db_session: Session, dummy_member: User, dummy_organization: Organization
-) -> Generator[TestClient, None, None]:
-    """
-    Test Client with a member request context.
-    """
-    fastapi_app.dependency_overrides[deps.get_request_context] = lambda: deps.RequestContext(
-        db_session=db_session,
-        user_id=dummy_member.id,
-        act_as=ActAsInfo(
-            organization_id=dummy_organization.id,
-            role=OrganizationRole.MEMBER,
-        ),
+def dummy_access_token_admin_act_as_member(dummy_admin: User) -> str:
+    org_membership = dummy_admin.organization_memberships[0]
+    return _sign_token(
+        dummy_admin,
+        ActAsInfo(organization_id=org_membership.organization_id, role=OrganizationRole.MEMBER),
     )
-    with TestClient(fastapi_app, base_url="http://localhost", follow_redirects=False) as c:
-        yield c
+
+
+@pytest.fixture(scope="function")
+def dummy_access_token_member(dummy_member: User) -> str:
+    org_membership = dummy_member.organization_memberships[0]
+    return _sign_token(
+        dummy_member,
+        ActAsInfo(organization_id=org_membership.organization_id, role=OrganizationRole.MEMBER),
+    )
+
+
+@pytest.fixture(scope="function")
+def dummy_access_token_no_orgs(dummy_user: User) -> str:
+    return _sign_token(dummy_user, None)
+
+
+# ------------------------------------------------------------
+# Dummy organization and user
+# - dummy_organization
+# - dummy_user
+# - dummy_admin (added to the dummy_organization as admin)
+# - dummy_member (added to the dummy_organization as member)
+# ------------------------------------------------------------
+
+
+@pytest.fixture(scope="function")
+def dummy_organization(db_session: Session) -> Organization:
+    dummy_organization = crud.organizations.create_organization(
+        db_session=db_session,
+        name="Dummy Organization",
+        description="Dummy Organization Description",
+    )
+    db_session.commit()
+    return dummy_organization
 
 
 @pytest.fixture(scope="function")
@@ -102,17 +95,6 @@ def dummy_user(db_session: Session, database_setup_and_cleanup: None) -> User:
     )
     db_session.commit()
     return dummy_user
-
-
-@pytest.fixture(scope="function")
-def dummy_organization(db_session: Session) -> Organization:
-    dummy_organization = crud.organizations.create_organization(
-        db_session=db_session,
-        name="Dummy Organization",
-        description="Dummy Organization Description",
-    )
-    db_session.commit()
-    return dummy_organization
 
 
 @pytest.fixture(scope="function")
