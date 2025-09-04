4731c4ee01db364fc63bbfcf8afe437578fd540a
Kelvin Wong <kelvin@aipolabs.xyz>
2025-09-04 16:27:58 +0100

    update comment

--- backend/aci/cli/commands/mock_data/create_mock_org_teams_users.py
+++ backend/aci/cli/commands/mock_data/create_mock_org_teams_users.py
@@ -91,7 +91,8 @@ def create_mock_org_teams_users(
         crud.teams.add_team_member(db_session, organization.id, team2.id, user2.id)
 
         # Important: not adding these configs to cli/config.py as these should only be used in local
-        # development. These should not be accessed on production deployment.
+        # development. These not be used by any other code in cli, and would not be accessible in
+        # actual deployment
         jwt_signing_key = utils.check_and_get_env_variable("JWT_SIGNING_KEY")
         jwt_algorithm = "HS256"
         jwt_access_token_expire_minutes = 1440
