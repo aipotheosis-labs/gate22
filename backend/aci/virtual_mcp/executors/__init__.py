59067b563c092dcd066940cbdb2780f851a6552b
fixer <14433035+thisisfixer@users.noreply.github.com>
2025-09-21 20:53:43 +0100

    wip: implement connector type tool

--- backend/aci/virtual_mcp/executors/__init__.py
+++ backend/aci/virtual_mcp/executors/__init__.py
@@ -2,6 +2,7 @@ from aci.common.db.sql_models import VirtualMCPTool
 from aci.common.enums import VirtualMCPToolType
 from aci.common.schemas.virtual_mcp import VirtualMCPToolMetadata
 from aci.virtual_mcp.executors.base_executor import ToolExecutor
+from aci.virtual_mcp.executors.connector_tool_executor import ConnectorToolExecutor
 from aci.virtual_mcp.executors.rest_tool_executor import RestFunctionExecutor
 
 
@@ -10,5 +11,5 @@ def get_tool_executor(tool: VirtualMCPTool) -> ToolExecutor:
     match tool_type:
         case VirtualMCPToolType.REST:
             return RestFunctionExecutor()
-        case _:
-            raise ValueError(f"Unsupported tool type: {tool_type}")
+        case VirtualMCPToolType.CONNECTOR:
+            return ConnectorToolExecutor()
